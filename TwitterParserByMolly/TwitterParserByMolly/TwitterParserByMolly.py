fcc230275e097fdf3d1b1f28ef6bdb61efa4285b
mollyBrennan <31749149+mollyBrennan@users.noreply.github.com>
2019-03-08 15:38:56 -0600

    Azure and SSMS Db connected

--- TwitterParserByMolly/TwitterParserByMolly/TwitterParserByMolly.py
+++ TwitterParserByMolly/TwitterParserByMolly/TwitterParserByMolly.py
@@ -10,41 +10,48 @@ from dateutil import parser
 import time
 import os
 import subprocess
+import pyodbc 
 
 #Our Credentials for Twitter API App
 consumer_key = "DrhJOGilzxVB8FvV5WYvJ5t11"
 consumer_secret= "MaWhCzZelZbBwa6Q6bqWM0meI8rqDmElPF03IdsfGNSuqpil4U"
 access_token= "1098313089630564352-D8UckNvBgVKQ63NOu6KYDksG7PUeyv"
 access_token_secret="1Xc10HWckHCbEzdWj9ilCarM2rngJXCWj2HVnmNPj9KY6"
-
-
+#Authentication to use Tweepy
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
+
+#Azure database credential details
+server = 'rateurrefserver.database.windows.net' 
+database = 'RateUrRefDb' 
+dbUsername = 'RateUrRef' 
+password = 'CSGals1234' 
 
 #Connecting to the columns of our database
 #list of hashtags and user mentions are going to need to be stored in their own tables and related to the tweet table
 def connect(tweet_id, text, username, created_at, user_location, place, retweet_count, favorite_count, verified, hashtags, user_mentions):
 	try:
-		con = mysql.connector.connect(host = 'localhost',
-		database= 'twitterdb', user='root', password = 'sesame', charset = 'utf8')
+		#con = mysql.connector.connect(host = 'localhost',
+		#database= 'twitterdb', user='root', password = 'sesame', charset = 'utf8')
+		cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+dbUsername+';PWD='+ password)
+		#pyodbc.connect(Driver={ODBC Driver 13 for SQL Server};Server=tcp:rateurrefserver.database.windows.net,1433;Database=RateUrRefDb;Uid=RateUrRef@rateurrefserver;Pwd={your_password_here};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;)
 
-		if con.is_connected():
-			cursor = con.cursor()
-			tweet_query = "INSERT INTO tweet_data_molly (tweet_id, text,username, created_at, user_location, place, retweet_count, favorite_count, verified) VALUES (%s,%s, %s, %s, %s, %s, %s, %s, %s)"
-			cursor.execute(tweet_query, (tweet_id, text, username, created_at, user_location, place, retweet_count, favorite_count, verified))
-			for aHashtag in hashtags: #Stores all hashtags from tweet into hashtag table
-			    hashtag_query = "INSERT INTO hashtags (tweet_id, hashtag) VALUES (%s,%s)"
-			    cursor.execute(hashtag_query, (tweet_id, aHashtag['text']))
-			for aUserMention in user_mentions: #Stores all user_mentions from tweet into user_mention table
-			    user_mention_query = "INSERT INTO user_mentions (tweet_id, user_mention) VALUES (%s,%s)"
-			    cursor.execute(user_mention_query, (tweet_id, aUserMention['name']))
-			con.commit()
+		cursor = cnxn.cursor()
+		tweet_query = "INSERT INTO [dbo].[tweets] (tweet_id, text,username, created_at, user_location, place, verified) VALUES (?, ?, ?, ?, ?, ?, ?)"
+		cursor.execute(tweet_query, (tweet_id, text, username, created_at, user_location, place, verified))
+		for aHashtag in hashtags: #Stores all hashtags from tweet into hashtag table
+			hashtag_query = "INSERT INTO [dbo].[hashtags] (tweet_id, hashtag) VALUES (?,?)"
+			cursor.execute(hashtag_query, (tweet_id, aHashtag['text']))
+		#for aUserMention in user_mentions: #Stores all user_mentions from tweet into user_mention table
+		#	user_mention_query = "INSERT INTO [dbo].[user_mentions]  (tweet_id, user_mention) VALUES (?,?)"
+		#	cursor.execute(user_mention_query, (tweet_id, aUserMention['name']))
+		cnxn.commit()
 	except Error as e: 
 			print(e)
 	
 	cursor.close()
-	con.close()
+	cnxn.close()
 
 	return
 
@@ -97,7 +104,7 @@ if __name__ == '__main__':
 	listener = Streamlistener(api = api)
 	stream = tweepy.Stream(auth, listener = listener)
 
-	track = ['ref','refs', 'referee', 'bad call', 'basketball', 'march madness']
+	track = ['ref','refs', 'referee', 'bad call', 'march madness']
 
 	#choose what we want to filter by 
 
